8d63daa46cb975b5838c01a4e56d337847d779c3
uku <uku3lig@gmail.com>
2022-09-26 19:09:05 +0200

    initial commit

---
+++ src/main/java/net/uku3lig/blessbottlerng/BlessBottleRNG.java
@@ -0,0 +1,17 @@
+package net.uku3lig.blessbottlerng;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlessBottleRNG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
