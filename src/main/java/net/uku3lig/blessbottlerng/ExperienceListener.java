6c80515978c5fd33aeaee2d5ca254c2064a9d9ae
uku <uku3lig@gmail.com>
2022-09-26 19:29:48 +0200

    add a config value for the amount

--- src/main/java/net/uku3lig/blessbottlerng/ExperienceListener.java
+++ src/main/java/net/uku3lig/blessbottlerng/ExperienceListener.java
@@ -3,10 +3,12 @@ package net.uku3lig.blessbottlerng;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ExpBottleEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class ExperienceListener implements Listener {
     @EventHandler
     public void onExpBottle(ExpBottleEvent event) {
-        event.setExperience(10);
+        int amount = JavaPlugin.getPlugin(BlessBottleRNG.class).getConfig().getInt("amount", 10);
+        event.setExperience(amount);
     }
 }
